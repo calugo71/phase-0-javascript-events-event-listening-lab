84b81f72be01847a6bd226490a6290b0b413e64c
Carlos Lugo <calugo71@gmail.com>
2022-07-21 09:02:08 -0400

    commit tree d3c2c8e82a083bf680d3c785f7d32bd4c07d7dea

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-function addingEventListener() {
+function addingEventListener(){ 
+
+const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
+undefined
+input.addEventListener('click', function() {
+    alert('I was clicked!');})
 }
